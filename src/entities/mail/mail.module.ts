95e972be1db56a0c748a2f4ad329709c56de071d
Igor Smelyanets <ismelyanets@yahoo.com>
2023-06-07 18:43:25 +0300

    add check unique email

--- src/entities/mail/mail.module.ts
+++ src/entities/mail/mail.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
+import { User } from '@entities/users/users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [ TypeOrmModule.forFeature([User])],
   controllers: [MailController],
-  providers: [MailService]
+  providers: [MailService],
+  exports:[MailService]
 })
 export class MailModule {}
