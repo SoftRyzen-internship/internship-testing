0eba1b630050e18fc752f735e20baf9dea860daa
AButsai <butsai@ukr.net>
2023-06-08 11:32:58 +0300

    added router check phone

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { TypeOrmModule } from '@db/typeorm.config';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from './config.module';
 import { AuthModule } from '@entities/auth/auth.module';
-import { UserModule } from '@entities/users/users.module';
 import { MailModule } from '@entities/mail/mail.module';
 import { QuestionsModule } from '@entities/questions/questions.module';
-import { PhoneModule } from './entities/phone/phone.module';
+import { UserModule } from '@entities/users/users.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from './config.module';
 
 @Module({
   imports: [
@@ -15,7 +14,6 @@ import { PhoneModule } from './entities/phone/phone.module';
     UserModule,
     MailModule,
     QuestionsModule,
-    PhoneModule,
   ],
   controllers: [],
   providers: [],
--- src/entities/auth/auth.controller.ts
+++ src/entities/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { PhoneDto } from './dto/phone.dto';
 
-@Controller('auth')
-export class AuthController {}
+@ApiTags('Authentication')
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiResponse({ status: 200, type: String, description: 'OK' })
+  @Post('check-phone')
+  async checkPhone(@Body() body: PhoneDto) {
+    return this.authService.checkPhone(body.phone);
+  }
+}
