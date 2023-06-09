65c7102afa97cdfc10f7015ebabe854e349ea527
Igor Smelyanets <ismelyanets@yahoo.com>
2023-06-09 17:31:07 +0300

    add static module

--- src/entities/auth/auth.module.ts
+++ src/entities/auth/auth.module.ts
@@ -4,11 +4,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from '@entities/users/users.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", 'public'),
+      serveRoot: '/avatars'
+    })
   ],
   controllers: [AuthController],
   providers: [AuthService, ],
