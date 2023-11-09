bc312e7e4e2ab893573ce38f6360411ccc4c3033
AButsai <butsai@ukr.net>
2023-11-09 04:53:56 +0200

    edit technical-test-result  updateTechnicalResultTest

--- src/entities/technical-test-result/technical-test-result.module.ts
+++ src/entities/technical-test-result/technical-test-result.module.ts
@@ -1,4 +1,5 @@
 import { TechnicalTestModule } from '@entities/technical-test/technical-test.module';
+import { UserEntity } from '@entities/users/users.entity';
 import { JwtGuardsModule } from '@guards/jwtGuard/jwt-guard.module';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -9,7 +10,7 @@ import { TechnicalTestResultService } from './technical-test-result.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ResultTechnicalTest]),
+    TypeOrmModule.forFeature([ResultTechnicalTest, UserEntity]),
     JwtGuardsModule,
     TechnicalTestModule,
   ],
