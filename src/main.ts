4cdf72e75464ac0d031db170dbe8b9ffdeb8bc9f
AButsai <butsai@ukr.net>
2023-12-20 01:52:04 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,12 @@ import { UploadDto } from '@entities/upload/dto/upload.dto';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { NextFunction, Request, Response } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
 
-  const app = await NestFactory.create(AppModule);
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Accept, Authorization, token-type',
-    );
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Internship Testing API')
