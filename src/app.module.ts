b69ceb27e7e02a39a0b60472ac551104227665d8
AButsai <butsai@ukr.net>
2024-02-14 23:57:02 +0200

    add email temlate

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TechnicalTestModule } from '@entities/technical-test/technical-test.mod
 import { TestsModule } from '@entities/testing/tests.module';
 import { UserModule } from '@entities/users/users.module';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-// import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER } from '@nestjs/core';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ConfigModule } from './config.module';
@@ -16,6 +16,7 @@ import { AttemptsModule } from './entities/attempts/attempts.module';
 import { BaseModule } from './entities/base/base.module';
 import { DashboardModule } from './entities/dashboard/dashboard.module';
 import { DirectionModule } from './entities/direction/direction.module';
+import { GoogleDriveModule } from './entities/google-drive/google-drive.module';
 import { GoogleModule } from './entities/google/google.module';
 import { MaterialsModule } from './entities/materials/materials.module';
 import { ParseXlsxModule } from './entities/parse-xlsx/parse-xlsx.module';
@@ -25,9 +26,8 @@ import { TasksModule } from './entities/tasks/tasks.module';
 import { TechnicalTestResultModule } from './entities/technical-test-result/technical-test-result.module';
 import { TokensModule } from './entities/tokens/tokens.module';
 import { UploadModule } from './entities/upload/upload.module';
-// import { GlobalExceptionFilter } from './logger/global-exception-filter';
-// import { GlobalLoggerService } from './logger/global-logger.service';
-import { GoogleDriveModule } from './entities/google-drive/google-drive.module';
+import { GlobalExceptionFilter } from './logger/global-exception-filter';
+import { GlobalLoggerService } from './logger/global-logger.service';
 import { CorsMiddleware } from './middlewares/cors-middleware';
 
 @Module({
@@ -62,13 +62,13 @@ import { CorsMiddleware } from './middlewares/cors-middleware';
     GoogleDriveModule,
   ],
   controllers: [],
-  // providers: [
-  //   {
-  //     provide: APP_FILTER,
-  //     useClass: GlobalExceptionFilter,
-  //   },
-  //   GlobalLoggerService,
-  // ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    GlobalLoggerService,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
